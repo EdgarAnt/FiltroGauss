d7e41f09e380708e8a3fe8ffd948e16814029fd5
EdgarAnt <eadv7777@gmail.com>
2023-03-18 12:28:44 -0600

    filter

---
+++ filtroGauss.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+#Direccion para leer la imagen
+imagenOriginal= cv.imread("C:\\Users\\Laptop\\Desktop\\bojji.jpg")
+plt.imshow(imagenOriginal)
